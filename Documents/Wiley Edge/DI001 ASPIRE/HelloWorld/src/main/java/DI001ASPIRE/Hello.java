e0cdc9ad305fd32758cb3a5afb3ef357a2e22535
annasambrook <annasambrook@gmail.com>
2022-10-11 13:47:42 +0100

    Hello World in NetBeans

---
+++ Documents/Wiley Edge/DI001 ASPIRE/HelloWorld/src/main/java/DI001ASPIRE/Hello.java
@@ -0,0 +1,13 @@
+/**
+ *@author Anna Sambrook
+ *email: annasambrook@gmail.com
+ *date: 11/10/2022
+ *purpose: My first Hello World program in an IDE
+ */
+package DI001ASPIRE;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Anna!");
+    }
+}
