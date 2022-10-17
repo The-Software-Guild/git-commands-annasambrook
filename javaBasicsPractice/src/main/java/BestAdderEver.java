3f403873e3c52f655cea4246bc148ae204702a67
annasambrook <annasambrook@gmail.com>
2022-10-17 15:41:16 +0100

    Best Adder Ever completed

---
+++ javaBasicsPractice/src/main/java/BestAdderEver.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Anna
+ */
+public class BestAdderEver {
+    public static void main(String[] args){
+        double numA;
+        double numB;
+        double numC;
+        
+        double total;
+        
+        numA = 5.5;
+        numB = 2.9;
+        numC = 9.3;
+        
+        total = numA + numB + numC;
+        System.out.println(total);
+    }
+}
