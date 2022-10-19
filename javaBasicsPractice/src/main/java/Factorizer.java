e348c2c97555ca42a2c0abcf2104f9a39c17b709
annasambrook <annasambrook@gmail.com>
2022-10-19 12:27:23 +0100

    Factorizer completed 2

--- javaBasicsPractice/src/main/java/Factorizer.java
+++ javaBasicsPractice/src/main/java/Factorizer.java
@@ -8,6 +8,46 @@
  *
  * @author Anna
  */
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Factorizer {
-    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        int number;
+        ArrayList<Integer> factors = new ArrayList<Integer>();
+        int factorsTotal = 0;
+        boolean isPerfect = false;
+        boolean isPrime = false;
+        
+        System.out.println("What number would you like to factor?");
+        number = scn.nextInt();
+        
+        for (int i = 0; i < number; i++){
+            if (number % (i +1) == 0){
+                factors.add(i + 1);
+            }
+        }
+        
+        for (int i = 0; i < factors.size() -1; i++){
+            factorsTotal += factors.get(i);
+        }
+        if (factorsTotal == number){
+            isPerfect = true;
+        }
+        
+        if (factors.get(1) == number){
+            isPrime = true;
+        }
+        
+        System.out.println("The factors of " + number + " are:");
+        System.out.println(factors);
+        System.out.println(number + " has " + factors.size() + " factors.");
+        System.out.println("This number is perfect: " + isPerfect);
+        System.out.println("This number is a prime number: " + isPrime);
+        
+
+    }
 }
